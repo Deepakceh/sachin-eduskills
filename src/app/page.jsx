40b82a07c6c41edc530207ffda3a3e6a316bca6d
deepaksapphiretech <deepak.kumar@sapphiretech.co.in>
2025-05-08 02:48:41 +0530

    update navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,17 @@
 import BannerSection from '../components/BannerSection'
-// import StatsSection from '../components/StatsSection'
 import AboutSection from '../components/AboutSection'
+import MissionSection from '../components/MissionSection'
 import ContactSection from '../components/ContactSection'
-// import CourseSection from '../components/CourseSection'
 import CurriculumSection from '../components/CurriculumSection'
-// import WhyChooseSection from '../components/WhyChooseSection'
-// import ServiceSection from '../components/ServiceSection'
-// import EnrollmentSection from '../components/EnrollmentSection'
 export default function page() {
 
   return (
     <>
       <BannerSection />
-      {/* <StatsSection /> */}
       <AboutSection animateOnScroll={true} />
+      <MissionSection />
       <CurriculumSection />
       <ContactSection />
-
-      {/* <WhyChooseSection /> */}
-      {/* <ServiceSection /> */}
-      {/* <CourseSection /> */}
-      {/* <EnrollmentSection /> */}
     </>
   )
 }
